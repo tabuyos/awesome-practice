f69641b08c863c3b9e19f41f69197c702d13d780
tabuyos <tabuyos@outlook.com>
2022-08-05 16:59:06 +0800

    add reactor and rxjava quickstart

---
+++ reactor/reactor-quickstart/src/main/java/com/tabuyos/reactor/quickstart/application/ReactorQuickstartApplication.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2018-2022 Tabuyos All Right Reserved.
+ */
+package com.tabuyos.reactor.quickstart.application;
+
+/**
+ * ReactorQuickstartApplication
+ *
+ * @author tabuyos
+ * @since 2022/8/5
+ */
+public class ReactorQuickstartApplication {
+
+  public static void main(String[] args) {
+    // Flux.just("java.math.BigDecimal", "java.time.Instant")
+    //     .map(Class::forName);
+  }
+
+}
---
+++ rxjava/rxjava-quickstart/src/main/java/com/tabuyos/rxjava/quickstart/application/RxjavaQuickstartApplication.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2018-2022 Tabuyos All Right Reserved.
+ */
+package com.tabuyos.rxjava.quickstart.application;
+
+/**
+ * RxjavaQuickstartApplication
+ *
+ * @author tabuyos
+ * @since 2022/8/5
+ */
+public class RxjavaQuickstartApplication {
+
+  public static void main(String[] args) {
+    // Flowable.just("java.math.BigDecimal", "java.time.Instant")
+    //         .map(Class::forName);
+  }
+
+}
